88d3630291330205e1073557e009949fd9c9c5e3
Eric Xue <ovo4096@gmail.com>
2020-07-19 14:28:54 +0800

    完善部分功能

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 
 const axiosInstance = axios.create();
 
+axiosInstance.defaults.headers.common['Wechat-Agent-Id'] = process.env.VUE_APP_WECHAT_AGENT_ID;
+
 Vue.prototype.$axios = axiosInstance;
 
 export default axiosInstance;
